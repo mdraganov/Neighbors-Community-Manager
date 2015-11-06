f29df0aaa64f16528b8ffc0660cef12f040dbaa9
vesheff <martin.veshev@gmail.com>
2015-11-06 21:52:30 +0200

    Delete metadata trash

--- Source/Server/NeighboursCommunitySystem.API/Controllers/ProposalsController.cs
+++ Source/Server/NeighboursCommunitySystem.API/Controllers/ProposalsController.cs
@@ -1,22 +1,5 @@
 ﻿namespace NeighboursCommunitySystem.API.Controllers
 {
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-    using NeighboursCommunitySystem.Services;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Net;
-    using System.Net.Http;
-    using System.Web.Http;
-
-
     public class ProposalsController
     {
 
