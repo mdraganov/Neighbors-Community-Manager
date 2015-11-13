98cbdc864382ca52f3de98beb9f0c61156636a2c
vesheff <martin.veshev@gmail.com>
2015-11-13 12:40:36 +0200

    Added signalR, and angular. SignalR working, angular just a skeleton. In a day or two will be done :)

--- Source/Data/NeighboursCommunitySystem.Models/Invitation.cs
+++ Source/Data/NeighboursCommunitySystem.Models/Invitation.cs
@@ -1,8 +1,9 @@
 ﻿namespace NeighboursCommunitySystem.Models
 {
-    using Common;
+    
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using Common;
 
     public class Invitation
     {
